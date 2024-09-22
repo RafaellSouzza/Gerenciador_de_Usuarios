15456d1b9a4a121f8cb8e9ed24a8687181ad6da3
Rafa Souza <31011616+RafaellSouzza@users.noreply.github.com>
2024-09-22 02:53:37 -0300

    Test

--- UsuariosAPI/src/main/java/com/infnet/UsuariosAPI/model/Usuario.java
+++ UsuariosAPI/src/main/java/com/infnet/UsuariosAPI/model/Usuario.java
@@ -6,13 +6,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
+import java.util.Date;
 
 @Entity
 @Data
 public class Usuario {
-
-   @Id
+    @Getter
+    @Setter
+    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
    private String nome;
---
+++ UsuariosAPI/src/test/java/com/infnet/UsuariosAPI/service/UsuariosServiceTest.java
@@ -0,0 +1,56 @@
+package com.infnet.UsuariosAPI.service;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import com.infnet.UsuariosAPI.model.Usuario;
+import com.infnet.UsuariosAPI.repository.UsuarioRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class UsuariosServiceTest {
+
+    @Mock
+    private UsuarioRepository usuariosRepository;
+
+    @InjectMocks
+    private UsuarioService usuariosService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testBuscarUsuarioPorId() {
+        Usuario mockUsuario = new Usuario();
+        mockUsuario.setId(1L);
+        mockUsuario.setNome("João");
+        mockUsuario.setSobrenome("Silva");
+        mockUsuario.setDataNascimento(LocalDate.of(1990, 1, 1));
+
+        when(usuariosRepository.findById(1L)).thenReturn(Optional.of(mockUsuario));
+
+        Optional<Usuario> resultado = usuariosService.pegarPorId(1L);
+
+        assertNotNull(resultado);
+        assertEquals("João", resultado.get().getNome());
+        assertEquals("Silva", resultado.get().getSobrenome());
+        assertEquals(LocalDate.of(1990, 1, 1), resultado.get().getDataNascimento());
+    }
+
+    @Test
+    public void testUsuarioNaoEncontrado() {
+        when(usuariosRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Optional<Usuario> resultado = usuariosService.pegarPorId(1L);
+
+        assertNull(resultado);
+    }
+}
